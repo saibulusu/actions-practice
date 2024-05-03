2a7b9e8dd71e881833256b0d8c78ae8bf30b850e
saibulusu <saibulusu@microsoft.com>
2024-05-02 19:14:10 -0700

    Change 2.0.18.

--- update-version.sh
+++ update-version.sh
@@ -1,3 +1,10 @@
+# for defining
+calculate_new_version() {
+    IFS="." read -a array <<< $1
+    echo array[2]
+}
+
+# for calling
 echo "Hello, World!"
 
 # current_yaml=$(git show HEAD:.pipelines/azure.pipelines.yml)
@@ -16,9 +23,10 @@ echo "previous version: $previous_version"
 git checkout HEAD
 
 if [ "$previous_version" == "$current_version" ]; then
-    echo "have to update version now"
+    echo "versions match, have to update now"
+    new_version=calculate_new_version $current_version
 else
-    echo "no need to update"
+    echo "version already updated, no need to update further"
 fi
 
 # main_version=$(echo "$main_yaml" | yq eval ".variables.version")
