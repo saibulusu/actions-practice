2ca3ea906278b975e207b6f163adfe07c0e8eb90
saibulusu <saibulusu@microsoft.com>
2024-05-02 18:29:27 -0700

    Change 2.0.10.

--- update-version.sh
+++ update-version.sh
@@ -3,7 +3,7 @@ echo "Hello, World!"
 current_yaml=$(git show HEAD:.pipelines/azure.pipelines.yml)
 previous_yaml=$(git show HEAD~1:.pipelines/azure.pipelines.yml)
 
-current_version=$(shyaml get-value variables.version < $current_yaml)
+current_version=$(shyaml get-value variables.version < "$current_yaml")
 # main_version=$(echo "$main_yaml" | yq eval ".variables.version")
 
 # echo "Current branch version: $current_version"
