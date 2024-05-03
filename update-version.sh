2239c01d0db2e7e28e1cf4c127e603b133b8af74
saibulusu <saibulusu@microsoft.com>
2024-05-02 18:53:05 -0700

    Change 2.0.15.

--- update-version.sh
+++ update-version.sh
@@ -4,14 +4,21 @@ echo "Hello, World!"
 # previous_yaml=$(git show HEAD~1:.pipelines/azure.pipelines.yml)
 
 current_version=$(yq '.variables.version' .pipelines/azure.pipelines.yml)
-echo "$current_version"
+echo "current version: $current_version"
 
 git stash
 git checkout HEAD~1
 previous_version=$(yq '.variables.version' .pipelines/azure.pipelines.yml)
-echo "$previous_version"
+echo "previous version: $previous_version"
 
 git checkout HEAD
+
+if ["$previous_version" = "$current_version"]; then
+    echo "have to update version now"
+else
+    echo "no need to update"
+fi
+
 # main_version=$(echo "$main_yaml" | yq eval ".variables.version")
 
 # echo "Current branch version: $current_version"
