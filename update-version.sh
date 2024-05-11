18beb0111570f00b0bc374ccf8629bd6ecbe8323
saibulusu <saibulusu@microsoft.com>
2024-05-10 21:57:20 -0700

    Using a version file.

--- update-version.sh
+++ update-version.sh
@@ -8,16 +8,14 @@ calculate_new_version(){
     echo "$major.$minor.$patch"
 }
 
-echo "Hello, World!"
-
-current_version=$(yq '.variables.version' .pipelines/azure.pipelines.yml)
+current_version=$(<VERSION)
 echo "current version: $current_version"
 
 git config --global advice.detachedHead false
 
 git stash
 git checkout HEAD~1
-previous_version=$(yq '.variables.version' .pipelines/azure.pipelines.yml)
+previous_version=$(<VERSION)
 echo "previous version: $previous_version"
 
 git checkout main
@@ -27,15 +25,11 @@ if [ "$previous_version" == "$current_version" ]; then
     new_version=$(calculate_new_version $current_version)
     echo "new version: $new_version"
 
-    key="version"
-    new_value=$new_version
-    yaml_file=".pipelines/azure.pipelines.yml"
-
-    sed -r "s/^(\\s*${key}\\s*:\\s*).*/\\1${new_value}/" -i "$yaml_file"
+    echo "$new_version" > VERSION
 
     git config user.email "sai.bulusu@gmail.com"
     git config user.name "saibulusu"
-    git add -f .pipelines/azure.pipelines.yml
+    git add -f VERSION
     git commit -m "Updating to $new_version"
     git push
 else
